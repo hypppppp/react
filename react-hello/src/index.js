81ab2241e61b17bc72e2e2224428c6eb7052877a
Yunpeng Hu <huyunpeng110@outlook.com>
2018-04-03 20:23:54 +0800

    first commit

---
+++ react-hello/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+function HelloWorld() {
+	return (
+	     <h1>hello</h1>
+	);
+	
+}
+
+ReactDOM.render(<HelloWorld/>,
+document.querySelector('#root'));
